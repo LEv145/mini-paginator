1c0531af294c3483e407145527d17a8712963091
LEv145 <40071054+LEv145@users.noreply.github.com>
2021-06-25 11:58:07 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+with open("README.md", "r") as f:
+    long_description = f.read()
+with open("requirements.txt") as f:
+    requirements = f.readlines()
+setuptools.setup(
+    name="mini_paginator",
+    version="1.2.1",
+    author="Lev145",
+    description="Mini-paginator for discord.py",
+    long_description=long_description,
+    url="https://github.com/LEv145/mini-paginator",
+    packages=setuptools.find_packages("mini_paginator"),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
